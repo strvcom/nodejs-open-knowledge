fe26c768305cc3b93531faf97fcf2839dec6152c
Samuel Prado <samuel.prado@strv.com>
2018-10-07 22:29:31 +0200

    Update repositories/dogs module

--- src/repositories/dogs.js
+++ src/repositories/dogs.js
@@ -1,24 +1,45 @@
 'use strict'
 
-const R = require('ramda')
 const errors = require('../utils/errors')
-const dogs = require('./../database/dogs.json')
+const { Dog } = require('../models')
 
+/**
+ * Returns all records
+ * @returns {Promisse<Array>}
+ */
 function findAll() {
-  return dogs
+  return Dog.query()
 }
 
-function findById(id) {
-  const dog = R.find(R.propEq('id', id), dogs)
+/**
+ * Return record by id
+ * @param {Number} id record id
+ * @return {Promise<Dog>}
+ */
+async function findById(id) {
+  const dog = await Dog.query()
+    .findById(id)
+
   if (!dog) {
     throw new errors.NotFoundError()
   }
   return dog
 }
 
-function create(dog) {
-  dog.id = dogs.length + 1
-  dogs.push(dog)
+/**
+ * Create record
+ * @param {Object} attributes Dog object
+ * @param {String} attributes.name Dog name
+ * @param {String} attributes.breed Dog breed
+ * @param {Date} attributes.birthYear Dog birth year
+ * @param {String} attributes.photo Dog photo
+ * @param {Number} attributes.userId Dog owner id
+ * @return {Promise<Dog>}
+ */
+async function create(attributes) {
+  const dog = await Dog.query()
+    .insertGraphAndFetch(attributes)
+
   return dog
 }
 
