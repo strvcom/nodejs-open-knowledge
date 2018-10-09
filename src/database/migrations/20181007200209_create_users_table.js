07b5c7508a7d0522317ffc2089cf59e4869b9c1b
Samuel Prado <samuel.prado@strv.com>
2018-10-09 13:41:42 +0200

    Update migrations to reflect the "required" param on models

--- src/database/migrations/20181007200209_create_users_table.js
+++ src/database/migrations/20181007200209_create_users_table.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: knex => knex.schema.createTable('users', table => {
     table.increments('id').primary()
-    table.string('email').unique()
-    table.string('name')
-    table.string('password')
+    table.string('email').unique().notNullable()
+    table.string('name').notNullable()
+    table.string('password').notNullable()
     table.boolean('disabled')
     table.timestamps()
   }),
--- src/database/migrations/20181007214622_create_dogs_tables.js
+++ src/database/migrations/20181007214622_create_dogs_tables.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: knex => knex.schema.createTable('dogs', table => {
     table.increments('id').primary()
-    table.string('name')
-    table.string('breed')
-    table.date('birth_year')
+    table.string('name').notNullable()
+    table.string('breed').notNullable()
+    table.date('birth_year').notNullable()
     table.text('photo')
     table.integer('user_id')
     table.foreign('user_id').references('users.id').onDelete('CASCADE')
