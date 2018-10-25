2ce2bae45af80b33f52aa35c424b91bb13461a86
Jiri Erhart <jiri.erhart@strv.com>
2018-10-25 17:58:47 +0200

    Remove authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,23 +2,14 @@
 
 const Router = require('koa-router')
 const { handleErrors, handleNotFound } = require('../middleware/errors')
-const { authenticate } = require('../middleware/authentication')
 const dogs = require('../controllers/dogs')
-const users = require('../controllers/users')
 
 const router = new Router()
 router.use(handleErrors)
 
-/* PUBLIC ROUTES */
-
-router.post('/session/user', users.login)
-router.post('/users', users.signUp)
-
-/* PRIVATE ROUTES */
-
-router.get('/dogs', authenticate, dogs.getAll)
-router.get('/dogs/:id', authenticate, dogs.getById)
-router.post('/dogs', authenticate, dogs.createDog)
+router.get('/dogs', dogs.getAll)
+router.get('/dogs/:id', dogs.getById)
+router.post('/dogs', dogs.createDog)
 
 router.use(handleNotFound)
 
